523e24534cafac90b896735c12fc237349b06a13
pizzetta gilles <gpizzetta@gmail.com>
2026-05-03 14:46:18 +0200

    fix menu

--- docs/functions.js
+++ docs/functions.js
@@ -1,5 +1,5 @@
 /** Doit rester aligné avec <Version> dans docs/manifest.xml */
-var ADDIN_VERSION = "1.0.71.0";
+var ADDIN_VERSION = "1.0.72.0";
 
 /**
  * Débogage (console.log) : PALO.DATAC / SETDATA tournent dans le runtime « Custom Functions »
