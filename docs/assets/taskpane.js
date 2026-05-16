ddadc1c5b69409792b334a46e8a9d89af60b04a7
pizzetta gilles <gpizzetta@gmail.com>
2026-05-16 19:24:53 +0200

    bump version

--- docs/assets/taskpane.js
+++ docs/assets/taskpane.js
@@ -1,5 +1,5 @@
 (function taskpaneBootstrap() {
-  var PLUGIN_VERSION = "1.0.1.116";
+  var PLUGIN_VERSION = "1.0.1.117";
   var manager = null;
 
   function setText(id, message) {
--- docs/commands.js
+++ docs/commands.js
@@ -319,7 +319,7 @@
       await storageSetJson(PICKER_STORAGE_KEY, pickerPayload);
 
       var dialogUrl = new URL("palo-ename-picker.html", window.location.href);
-      dialogUrl.searchParams.set("v", "1.0.1.116");
+      dialogUrl.searchParams.set("v", "1.0.1.117");
 
       Office.context.ui.displayDialogAsync(
         dialogUrl.href,
